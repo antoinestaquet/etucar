223031e4eef5c4ca96bfb20fa8861e3c10249a85
Taumicron <73892287+Taumicron@users.noreply.github.com>
2022-01-17 18:31:23 +0100

    Changements dans le script JS

--- scripts/reset-mdp.js
+++ scripts/reset-mdp.js
@@ -1,5 +1,13 @@
-var formEmail = document.getElementById('email')
-var formCode = document.getElementById('code')
-var formReset = document.getElementById('reset')
-const btnEmail = document.getElementById('')
+const formEmail = document.getElementById('email')
+const formCode = document.getElementById('code')
+const formReset = document.getElementById('reset')
 
+const btnEmail = document.getElementById('btnemail')
+
+btnEmail.onclick = function() {
+    var mail = new String(document.getElementById('email').value)
+    if (mail.length > 2) {//à remplacer par "si le serveur retourne vrai car l'email correspond à un compte"
+        var bsCollapse = new bootstrap.Collapse(formEmail, {hide : true})
+        console.log("ok")
+    }
+}
