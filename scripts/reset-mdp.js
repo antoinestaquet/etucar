b863bdff19f29eb3a0181a7713459be1bf2bcdb9
Taumicron <73892287+Taumicron@users.noreply.github.com>
2022-01-17 18:05:36 +0100

    Implémentation de mot-de-passe-oublie.html
    Et début du script js qui gère la page

---
+++ scripts/reset-mdp.js
@@ -0,0 +1,5 @@
+var formEmail = document.getElementById('email')
+var formCode = document.getElementById('code')
+var formReset = document.getElementById('reset')
+const btnEmail = document.getElementById('')
+
