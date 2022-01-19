eb889a1e867d0781c0aa7598fca8ea83969e4fe7
Antoine Staquet <70856482+antoinestaquet@users.noreply.github.com>
2022-01-19 16:31:26 +0100

    Ajout de la validation
    - Changement des boutons de button à submit
    - Changement des onclicks en eventlistener sur le submit des forms

--- scripts/reset-mdp.js
+++ scripts/reset-mdp.js
@@ -1,40 +1,67 @@
+/**
+ *  if(!formEmail.checkValidity())
+ * preventDefault() permet : d'éviter de submit le formulaire
+ * et par conséquent permet d'éviter le refresh de la page 
+ * (cela risque de poser problème car à chaque submit on reviendrait
+ * à l'état initial => possible solution : réaliser l'opération dynamiquement
+ * côté serveur)
+ */
+
 const formEmail = document.getElementById('email')
 const formCode = document.getElementById('code')
 const formReset = document.getElementById('reset')
 
-const btnEmail = document.getElementById('btnemail')
-const btnCode = document.getElementById('btncode')
-const btnReset = document.getElementById('btnreset')
-
-btnEmail.onclick = function() {
+formEmail.addEventListener('submit', (e) => {
     var mail = document.getElementById('emailtxt').value
-    if (true) {//à remplacer par "si le serveur retourne vrai car l'email correspond à un compte"
+    e.preventDefault();
+    if (formEmail.checkValidity()) {//à remplacer par "si le serveur retourne vrai car l'email correspond à un compte"
         formEmail.classList.add("d-none")
         formCode.classList.remove("d-none")
     }
-}
+    formEmail.classList.add("was-validated")
+})
 
-btnCode.onclick = function() {
+formCode.addEventListener('submit', (e) => {
     var code = document.getElementById('txtcode').value
-    if (true) {//à remplacer par "si le serveur retourne vrai car le code est le bon"
+    //if(!formEmail.checkValidity())
+    e.preventDefault()
+    if (formCode.checkValidity()) {//à remplacer par "si le serveur retourne vrai car le code est le bon"
         formCode.classList.add("d-none")
         formReset.classList.remove("d-none")
         formReset.classList.add("d-display")
     }
+    formCode.classList.add("was-validated")
+})
+
+function validationMotDePasse() {
+    var mdp = document.getElementById('mdp')
+    var verif = document.getElementById('verif')
+    if(mdp.value.localeCompare(verif.value) != 0){
+        mdp.setCustomValidity("Les mots de passes ne correspondent pas.")
+        verif.setCustomValidity("Les mots de passes ne correspondent pas.")
+    } else {
+        mdp.setCustomValidity("")
+        verif.setCustomValidity("")
+    }
 }
 
-btnReset.onclick = function() {
-    var mdp = document.getElementById('mdp').value
-    var verif = document.getElementById('verif').value
+formReset.addEventListener('submit', (e) => {
+
     var err = document.getElementById('mdperr')
-    if (mdp.localeCompare(verif) == 0){ //Implémenter vérification serveur
+    //if(!formEmail.checkValidity())
+    e.preventDefault()
+    document.getElementById('mdp').oninput = validationMotDePasse
+    document.getElementById('verif').oninput = validationMotDePasse
+    validationMotDePasse()
+
+    if (formReset.checkValidity()) { //Implémenter vérification serveur
         var btnRedirection = document.getElementById('redirection')
-        err.classList.add("d-none")
         btnRedirection.classList.remove("d-none")
         setTimeout(() => {
             window.location.replace("connexion.html")
         }, 5000);
-    } else {
-        err.classList.remove("d-none")
     }
-}
+    formReset.classList.add("was-validated")
+})
+
+
