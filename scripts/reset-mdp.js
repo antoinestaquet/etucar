1f21ea5853006ff3c5d027dad1a1a3be70f79e9d
Taumicron <73892287+Taumicron@users.noreply.github.com>
2022-01-19 10:28:53 +0100

    amélioration du script JS

--- scripts/reset-mdp.js
+++ scripts/reset-mdp.js
@@ -3,11 +3,30 @@ const formCode = document.getElementById('code')
 const formReset = document.getElementById('reset')
 
 const btnEmail = document.getElementById('btnemail')
+const btnCode = document.getElementById('btncode')
+const btnReset = document.getElementById('btnreset')
 
 btnEmail.onclick = function() {
-    var mail = new String(document.getElementById('email').value)
+    var mail = document.getElementById('emailtxt').value
     if (mail.length > 2) {//à remplacer par "si le serveur retourne vrai car l'email correspond à un compte"
-        var bsCollapse = new bootstrap.Collapse(formEmail, {hide : true})
-        console.log("ok")
+        formEmail.classList.add("d-none")
+        formCode.classList.remove("d-none")
+    }
+}
+
+btnCode.onclick = function() {
+    var code = document.getElementById('txtcode').value
+    if (true) {//à remplacer par "si le serveur retourne vrai car le code est le bon"
+        formCode.classList.add("d-none")
+        formReset.classList.remove("d-none")
+        formReset.classList.add("d-display")
+    }
+}
+
+btnReset.onclick = function() {
+    var mdp = document.getElementById('mdp')
+    var verif = document.getElementById('verif')
+    if (mdp == verif){
+        
     }
 }
